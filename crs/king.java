2cc330ba1e5003b43e1bbeb0bf07d7585b167627
teekingtv <37657628+teekingtv@users.noreply.github.com>
2018-03-28 04:46:44 +0100

    Create king.java

---
+++ crs/king.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class king {
+
+	public static void main(String[] args) {
+	
+		tee myLayout = new tee();
+		myLayout.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+
+}
